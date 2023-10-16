1586f7fd4838c9a7bfdd2d453dd01106a10cb4b1
subasree <148018836+subasreedev@users.noreply.github.com>
2023-10-15 19:23:03 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,80 @@
+TweenMax.from(".logo",1,{
+    opacity:0,
+    x:-20,
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".menu-links ul li",1,{
+    opacity:0,
+    x:-20,
+    ease:Power3.easeInOut
+}, 0.08)
+
+TweenMax.from(".search",1,{
+    delay:.5,
+    opacity:0,
+    x:-20,
+    ease:Power3.easeInOut
+}, 0.08)
+
+TweenMax.from(".account",1,{
+    delay:.6,
+    opacity:0,
+    x:-20,
+    ease:Power3.easeInOut
+}, 0.08)
+
+TweenMax.from(".cart",1,{
+    delay:.7,
+    opacity:0,
+    x:-20,
+    ease:Power3.easeInOut
+}, 0.08)
+
+TweenMax.from(".cupcake",1,{
+    delay:2,
+    opacity:0,
+    y:-800,
+    ease:Power3.easeInOut
+}, 0.08)
+
+TweenMax.from(".title",1,{
+    delay:1,
+    opacity:0,
+    y:20,
+    ease:Power3.easeInOut
+}, 0.08)
+
+TweenMax.from(".tagline",1,{
+    delay:1.3,
+    opacity:0,
+    y:20,
+    ease:Power3.easeInOut
+}, 0.08)
+
+TweenMax.from(".pages",1,{
+    delay:1.3,
+    opacity:0,
+    y:20,
+    ease:Power3.easeInOut
+}, 0.08)
+
+TweenMax.from(".more",1,{
+    delay:1.4,
+    opacity:0,
+    y:20,
+    ease:Power3.easeInOut
+}, 0.08)
+
+TweenMax.from(".desc",1,{
+    delay:1.3,
+    opacity:0,
+    y:20,
+    ease:Power3.easeInOut
+}, 0.08)
+
+TweenMax.from(".arrows",1,{
+    delay:2,
+    opacity:0,
+    ease:Power3.easeInOut
+}, 0.08)
